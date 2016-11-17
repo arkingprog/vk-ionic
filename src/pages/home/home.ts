b02c12844be9aafe84891cd4736605dbeb4ff588
arkingprog <arking@ukr.net>
2016-11-17 18:29:47 +0200

    add home component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import {VK} from '../../providers/vk';
-
+import { VK } from '../../providers/vk';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -8,11 +7,10 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController,public vk:VK) {
-    this.vk.request(
-
-    ).subscribe(data=> console.log(data));
+  constructor(public navCtrl: NavController, public vk: VK) {
+    // this.vk.request('users.get',{appid:'das',user_ids:['1']}).subscribe(data=>console.log(data))
+    this.vk.request('users.get', { appid: 23 }).subscribe(data => console.log(data))
+    // this.vk.request( ).subscribe(data=> console.log(data));
   }
-
 }
+
