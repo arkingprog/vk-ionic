4812693c38771bc826e1c79db4e8e75eec241095
arkingprog <arking@ukr.net>
2016-11-17 18:27:21 +0200

    add vk init func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,40 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-// import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
-
+import { VK, VKInitParams } from "../providers/vk";
+import { MainPage } from "../pages/main/main";
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  templateUrl: `app.html`
 })
 export class MyApp {
-  rootPage = LoginPage;
+  rootPage;
+  constructor(platform: Platform, public vk: VK) {
+    let self = this;
+    
+    let vkInitParam: VKInitParams = {
+      client_id: '5729897',
+      display: 'mobile',
+      redirect_uri: 'https://oauth.vk.com/blank.html',
+      response_type: 'token',
+      v: '5.60'
+    }
 
-  constructor(platform: Platform) {
     platform.ready().then(() => {
+
+      self.vk.init(vkInitParam);
+
+
+      if (self.vk.isAuth)
+        self.rootPage = MainPage
+      else
+        self.rootPage = LoginPage;
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
   }
+
 }
