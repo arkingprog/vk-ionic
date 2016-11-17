4b58c07a7d05cbc071eb797a44a3cfcf6ea1f458
arkingprog <arking@ukr.net>
2016-11-17 21:35:21 +0200

    add response User interface

--- src/lib/types/user.type.ts
+++ src/lib/types/user.type.ts
@@ -1,3 +1,8 @@
-export interface user{
-
+export interface responseUser{
+    id:number;
+    first_name:string;
+    last_name:string;
+    about?:string;
+    activities?:string;
+    bdate?:string;
 }
