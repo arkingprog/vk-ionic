127725e3d04961693470cc66d5ffd37f96657719
arkingprog <arking@ukr.net>
2016-11-29 19:38:30 +0200

    add userheader component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import {NgModule} from '@angular/core';
-import {IonicApp, IonicModule} from 'ionic-angular';
-import {MyApp} from './app.component';
-import {HomePage} from '../pages/home/home';
-import {LoginPage} from '../pages/login/login';
-import {VK} from "../providers/vk";
-import {MainPage} from "../pages/main/main";
+import { UserHeaderComponent } from './../components/user-header/user-header';
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { VK } from "../providers/vk";
+import { MainPage } from "../pages/main/main";
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     LoginPage,
-    MainPage
+    MainPage,
+    UserHeaderComponent
   ],
   imports: [
     IonicModule.forRoot(MyApp)
--- src/components/user-header/user-header.ts
+++ src/components/user-header/user-header.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
 
-/*
-  Generated class for the UserHeader component.
-
-  See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
-  for more info on Angular 2 Components.
-*/
 @Component({
   selector: 'user-header',
   templateUrl: 'user-header.html'
@@ -15,8 +9,7 @@ export class UserHeaderComponent {
   text: string;
 
   constructor() {
-    console.log('Hello UserHeader Component');
-    this.text = 'Hello World';
+
   }
 
 }
