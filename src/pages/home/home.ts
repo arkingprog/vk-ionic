a2db4c82fb319ad205a5517454ceb64ab3aca6dd
arkingprog <arking@ukr.net>
2016-11-29 23:59:54 +0200

    rename request to api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   constructor(public navCtrl: NavController, public vk: VK) {
     // this.vk.request('users.get',{appid:'das',user_ids:['1']}).subscribe(data=>console.log(data))
-    this.vk.request('users.get', { appid: 23 }).subscribe(data => console.log(data))
+    this.vk.api('users.get', { appid: 23 }).subscribe(data => console.log(data))
     // this.vk.request( ).subscribe(data=> console.log(data));
   }
 }
