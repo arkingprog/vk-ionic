a06ed713a586e7b89664ed9f3c0d4f541463edc4
arkingprog <arking@ukr.net>
2016-12-10 20:48:50 +0200

    update access token

--- src/providers/vk.ts
+++ src/providers/vk.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/operator/map';
 
 import { InAppBrowser } from "ionic-native";
 
-let access = '0230c9b1485ba126054040b8e2d1f084cc79ce999bece84ed5cfc4025ee0fee14bae9b147c154e1081750';
+let access = '9640b7bd15edf581d180245e386064df2af603c309e3bd3ef290ff467a0e03968b074bb063b943499b893';
 
 // https://api.vk.com/method/users.get?access_token=0230c9b1485ba126054040b8e2d1f084cc79ce999bece84ed5cfc4025ee0fee14bae9b147c154e1081750&fields=photo_id, verified, sex, bdate, city, country, home_town, has_photo, photo_50, photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig, online, lists, domain, has_mobile, contacts, site, education, universities, schools, status, last_seen, followers_count, common_count, occupation, nickname, relatives, relation, personal, connections, exports, wall_comments, activities, interests, music, movies, tv, books, games, about, quotes, can_post, can_see_all_posts, can_see_audio, can_write_private_message, can_send_friend_request, is_favorite, is_hidden_from_feed, timezone, screen_name, maiden_name, crop_photo, is_friend, friend_status, career, military, blacklisted, blacklisted_by_me
 
@@ -129,3 +129,7 @@ export enum VKScope {
   stats = 1048576,
   market = 1048576,
 }
+
+//https://oauth.vk.com/authorize?client_id=5729897&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=notify,friends,photos,audio,video,pages,status,notes,messages,wall,ads,offline,docs,groups,notifications,stats,market&response_type=token&v=5.60
+
+//notify,friends,photos,audio,video,pages,status,notes,messages,wall,ads,offline,docs,groups,notifications,stats,market
