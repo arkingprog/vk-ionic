4b7a32580b74f2b58e9adac2f4fe4d31b295eb0c
arkingprog <arking@ukr.net>
2016-12-10 18:57:01 +0200

    add user online pipe

---
+++ src/pipes/user-online.ts
@@ -0,0 +1,16 @@
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+
+@Pipe({
+  name: 'userOnline',
+  pure: false
+})
+@Injectable()
+export class UserOnlinePipe implements PipeTransform {
+  transform(value: number, lastSeen: any): string {
+    moment.locale('ru');
+    if (lastSeen) {
+      return value > 0 ? 'Онлайн' : `Последний раз был ${moment(lastSeen * 1000).fromNow()}`;
+    }
+  }
+}
