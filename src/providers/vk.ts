70c332067132e92eb9aa7f34e77393571fbbdd87
arkingprog <arking@ukr.net>
2016-12-11 10:02:03 +0200

    fix access token

--- src/providers/vk.ts
+++ src/providers/vk.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/operator/map';
 
 import { InAppBrowser } from "ionic-native";
 
-let access = '9640b7bd15edf581d180245e386064df2af603c309e3bd3ef290ff467a0e03968b074bb063b943499b893';
+let access = '<YOUR-ACCESS-TOKEN>';
 
 // https://api.vk.com/method/users.get?access_token=0230c9b1485ba126054040b8e2d1f084cc79ce999bece84ed5cfc4025ee0fee14bae9b147c154e1081750&fields=photo_id, verified, sex, bdate, city, country, home_town, has_photo, photo_50, photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig, online, lists, domain, has_mobile, contacts, site, education, universities, schools, status, last_seen, followers_count, common_count, occupation, nickname, relatives, relation, personal, connections, exports, wall_comments, activities, interests, music, movies, tv, books, games, about, quotes, can_post, can_see_all_posts, can_see_audio, can_write_private_message, can_send_friend_request, is_favorite, is_hidden_from_feed, timezone, screen_name, maiden_name, crop_photo, is_friend, friend_status, career, military, blacklisted, blacklisted_by_me
 
