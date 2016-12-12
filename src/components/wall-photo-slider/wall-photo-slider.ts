cf22a6469483cc1d9e96c1a2384d5dfdb5da3d93
arkingprog <arking@ukr.net>
2016-12-12 21:30:58 +0200

    create wall-photo-slider

--- src/components/wall-photo-slider/wall-photo-slider.ts
+++ src/components/wall-photo-slider/wall-photo-slider.ts
@@ -1,22 +1,40 @@
 import { VK } from './../../providers/vk';
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'wall-photo-slider',
   templateUrl: 'wall-photo-slider.html'
 })
-export class WallPhotoSliderComponent {
-
+export class WallPhotoSliderComponent implements AfterViewInit {
   @Input() count: number;
   photos: any[];
+  swipeOptions: any;
+
   constructor(public vk: VK) {
+    this.swipeOptions = {
+      slidesPerView: 'auto',
+      spaceBetween: 2, 
+      freeMode: true,
+      zoom: false,
+    };
+
 
     this.vk.api('photos.getAll',
-      {'skip_hidden':1,'count':25}).subscribe(data => { 
-        this.photos=data.items;
-        console.log(this.photos)
+      { 'skip_hidden': 1, 'count': 25 }).subscribe(data => {
+        this.photos = data.items;
+        this.photos.forEach((value, index, array) => {
+          value.x = this.calcNewSize(value.width,value.height,100,604);
+        })
       });
+  }
+
+  private calcNewSize(width: number, height: number, heightContainer: number, sizeVKPhoto: number): number {
+    let temp = sizeVKPhoto * height / width;
+    let x = heightContainer * sizeVKPhoto / temp;
+    return Math.round(x);
+  }
+
+  ngAfterViewInit() {
   }
 
 }
